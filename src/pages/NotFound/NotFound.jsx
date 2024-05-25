ae2a01bfbc0b5e8a0859bd09e32a865840ce82bb
EnChyb <144510772+EnChyb@users.noreply.github.com>
2024-05-26 01:51:49 +0200

    finished after bug

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,9 @@
 import { Link } from "react-router-dom"
-import css from "./NotFound.module.css"
 
 export const NotFound = () => {
     return (
         <>
-        <h2>Oops! Page not found!</h2>
+        <h2 >Oops! Page not found!</h2>
         <p>come back to <Link to="/">Homepage</Link></p>
         </>
     )    
